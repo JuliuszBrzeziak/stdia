db08c87be3a19fcc35724f36e7e6f258f0321e70
juliusz <juliusz.brzeziak@student.uj.edu.pl>
2020-10-24 15:45:32 +0200

    nonstatic decoder

--- java/decoder/Decoder.java
+++ java/decoder/Decoder.java
@@ -1,48 +1,48 @@
 
 
 
-public abstract class Decoder {
-	static int times = 0;
-	static String bufer="";
-	static String pod="";
-	static boolean pd = false;
-	static boolean z = false;
+
+public class Decoder {
+	int times = 0;
+  	String bufer="";
+  	String pod="";
+  	boolean pd = false;
+
+	int c = 3;
+
+	public void f(){
+		System.out.println(c);
+		
+	}
 
 
 
-	/**
-	 * Metoda pozwala na przekazanie ciągu znaków, który ma zostać odkodowany.
-	 * Sposób odkodowywania znaków przedstawiony jest w opisie zadania.
-	 *
-	 * @see <a href="https://zti.if.uj.edu.pl/Piotr.Oramus/dydaktyka/Java/Zadania/">Strona WWW kursu</a>
-	 * @param input ciąg do odkodowania
-	 */
-	//public abstract void decode(String input);
+	public  String getBuffer(){
+		if(this.bufer==null){
+			return "";
+		}
 
-	/**
-	 * Metoda umożliwia pobranie ciągu znaków znajdującego się w buforze. Działanie
-	 * metody nigdy nie może doprowadzić do pojawienia się wyniku null. Gdy brak
-	 * znaków w buforze metoda zwraca pusty ciąg ("").
-	 * 
-	 * @return aktualny stan bufora
-	 */
-	public abstract String getBuffer();
+		else{
+			return this.bufer;
+		}
+	}
 
 	/**
 	 * Metoda resetuje dekoder do stanu początkowego. Wszystkie znaki w buforze są
 	 * kasowane. Informacje przekazane do chwili wykonania tej metody mają zostać
 	 * zapomniane (np. informacja o tym, że nadawany jest podciąg).
 	 */
-	public static void reset(){
-		times = 0;
-		bufer = "";
+	public  void reset(){
+		this.times = 0;
+		this.bufer = "";
 	}
 
 
 
 
 
-	public static void decode(String input){
+	public  void decode(String input){
+		getBuffer();
 		StringBuilder helper = new StringBuilder(input);
 		StringBuilder helperpod = new StringBuilder("");
 
@@ -57,46 +57,46 @@ public abstract class Decoder {
 					return;
 				}
 				 //01 false
-				else if(helper.charAt(i+1)=='1' && pd == false){
-					pd = true;
+				else if(helper.charAt(i+1)=='1' && this.pd == false){
+					this.pd = true;
 					i=i+1;
 				}
 				//01 true
-				else if(helper.charAt(i+1)=='1' && pd == true){
-					pd = false;
-					for(int i3 = times; i3 > 0; i3--){
-						bufer = bufer + pod;
+				else if(helper.charAt(i+1)=='1' && this.pd == true){
+					this.pd = false;
+					for(int i3 = this.times; i3 > 0; i3--){
+						this.bufer = this.bufer + this.pod;
 					}
-					times = 0;
-					pod = "";
+					this.times = 0;
+					this.pod = "";
 					i=i+1;
 				}
 
-				else if(times != 0 && pd == false){
+				else if(this.times != 0 && this.pd == false){
 					int n = i;
 					n = n-1;
 					
 				}
 				//0x
 				else {
-					times = helper.charAt(i+1)-'0';
+					this.times = helper.charAt(i+1)-'0';
 					i=i+1;
 				}
 				
 			}
 			//01
-			else if(pd==true){
-				pod = pod + helper.charAt(i);
+			else if(this.pd==true){
+				this.pod = this.pod + helper.charAt(i);
 			}
 
 			else{
-				if(times != 0 && pd==false){
-					for(int i3 = times; i3 > 0; i3--){
-						bufer = bufer + helper.charAt(i);
+				if(this.times != 0 && this.pd==false){
+					for(int i3 = this.times; i3 > 0; i3--){
+						this.bufer = this.bufer + helper.charAt(i);
 					}
-					times = 0;
+					this.times = 0;
 				}
-				bufer = bufer + helper.charAt(i);
+				this.bufer = this.bufer + helper.charAt(i);
 			}
 
 		}
@@ -107,12 +107,14 @@ public abstract class Decoder {
 	public static void main(String[] args) {
 		String a="123";
 		System.out.println(a.length());
-		decode("qw003abc01d");
-		System.out.println("pr:"+bufer);
-		decode("zxcxv");
-		System.out.println("pr:"+bufer);
-		decode("01");
-		System.out.println("pr:"+bufer);
+		Decoder dec = new Decoder();
+		dec.f();
+		dec.decode("qw03abc01d");
+		System.out.println("pr:"+dec.bufer);
+		dec.decode("zxcxv");
+		System.out.println("pr:"+dec.bufer);
+		dec.decode("01");
+		System.out.println("pr:"+dec.bufer);
 
 		
 	}
