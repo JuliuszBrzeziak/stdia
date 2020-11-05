540c6bc32b310377dfb50cb332c0dfa7aedcd4fe
juliusz <juliusz.brzeziak@student.uj.edu.pl>
2020-11-05 22:24:19 +0100

    data process

--- alg/zad1/gen.cpp
+++ alg/zad1/gen.cpp
@@ -2,16 +2,16 @@
 #include <string>
 
 
+using namespace std;
 
 int main() {
 
     int n = 100;
+    cout << n << endl;
+
+    int a = 2;
+    cout << a << endl;
 
 
-
-    std::cout << "gens" << std::endl;
-    std::cout << "gen2" ;
-
-    std::cout << "gen2";
     return 0;
 }
--- alg/zad1/stdin.cpp
+++ alg/zad1/stdin.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main() {
-    for (std::string line; std::getline(std::cin, line);) {
-        std::cout << "stdin"<<line << std::endl;
+    for (string line; getline(cin, line);) {
+        // int l = atoi(line);
+        const char *t = line.c_str();
+        int j = atoi(t);
+
+        for(int i = 0; i < j; i++){
+            cout<< i ;
+        }
+        cout << "stdin"<<line << endl;
     }
     return 0;
 }
